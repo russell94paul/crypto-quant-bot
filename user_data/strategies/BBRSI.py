c4746658ef737bfe4956b7d6becd9b8a80036ad3
Ubuntu <ubuntu@ip-172-31-50-209.ec2.internal>
2021-06-05 20:21:55 +0000

    Populated repo with initial example strategies

---
+++ user_data/strategies/BBRSI.py
@@ -0,0 +1,46 @@
+# --- Do not remove these libs ---
+from freqtrade.strategy.interface import IStrategy
+from pandas import DataFrame
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+
+# --------------------------------
+
+
+class BBRSI(IStrategy):
+    """
+    author@: Gert Wohlgemuth
+    converted from:
+    https://github.com/sthewissen/Mynt/blob/master/src/Mynt.Core/Strategies/BbandRsi.cs
+    """
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+
+        # Bollinger bands
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=1)
+        dataframe['bb_lowerband'] = bollinger['lower']
+        dataframe['bb_middleband'] = bollinger['mid']
+        dataframe['bb_upperband'] = bollinger['upper']
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                    (dataframe['rsi'] < 17) &
+                    (dataframe['close'] < dataframe['bb_lowerband'])
+
+            ),
+            'buy'] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                    (dataframe['rsi'] > 96) &
+                     (dataframe['close'] > dataframe['bb_middleband'])
+
+            ),
+            'sell'] = 1
+        return dataframe
